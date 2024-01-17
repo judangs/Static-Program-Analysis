72cf6294171fef0bbd7c3029201665fc5c188713
It2time <wneotjd228@naver.com>
2024-01-18 01:01:58 +0900

    Update

--- analyzer.py
+++ analyzer.py
@@ -5,6 +5,7 @@ from source import disassembler
 from argparse import ArgumentParser
 from typing import ParamSpecArgs
 
+
 def main(arg: ParamSpecArgs):
     parse = parser.ElfParser(arg.file)
     disasm = disassembler.Disassembler(parse._io, parse.parser, parse.section_idx, parse.section_addr)
@@ -21,31 +22,45 @@ def main(arg: ParamSpecArgs):
     disassembler.BuildControlFlow(disasm)
 
 
-    print(disassembler.CanReachable(disasm, function['_start'], 0x40120e)) #None
-    print(disassembler.CanReachable(disasm, function['main'], 0x40120e)) #True
-    print(disassembler.CanReachable(disasm, function['main'], 0x4011fd)) #True
-
-    dest = 0x4011a5
-    path = list()
-    disassembler.TraceControlFlow(disasm, function['main'], dest, path)
-    for controlflow in disasm.ControlFlow:
-        for address in controlflow:
-            print("0x%x" %(address), end=' -> ')
-        print("0x%x" %(dest))
-
-    
-
-    '''
-    analyzer handler
-    ex)
+    #analyzer handler
     while True:
         print('(handler) ', end='')
-        intput()
-        ...
-    '''
+        user_input = input()
 
-    
-    
+        #(handler) testing.
+        if user_input.startswith("trace list"):
+            if not disasm.ControlFlow:
+                continue
+            disassembler.ControlFlowList(disasm, dest)
+
+        elif user_input.startswith("trace print"):
+            if not disasm.ControlFlow:
+                continue
+            command_parts = user_input.split(' ')
+            idx = int(command_parts[2], 16)
+            disassembler.PrintAssembleFlow(disasm, idx)
+
+
+        elif user_input.startswith("trace"):
+            disasm.ControlFlow.clear()
+            command_parts = user_input.split(' ')
+            
+            if len(command_parts) > 2:
+                start = int(command_parts[1], 16)
+                dest = int(command_parts[2], 16)
+                path = list()
+                disassembler.TraceControlFlow(disasm, start, dest, path)
+
+            else:
+                dest = int(command_parts[1], 16)
+
+                path = list()
+                for name, address in function.items():
+                    if disasm.IsExecutableAddr(address):
+                        disassembler.TraceControlFlow(disasm, function[name], dest, path)
+                        path = list()
+        
+
 
 
 if __name__ == "__main__":
